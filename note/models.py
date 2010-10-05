d06bf406da3a81b7b0833f58cba3d63ac29867ed
Feliks <felix.teachme@gmail.com>
2010-10-05 10:18:36 +0300

    second commit

---
+++ note/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+class Events(models.Model):
+	event = models.CharField(max_length=500)
+
+class Masters(models.Model):
+	master = models.CharField(max_length=30)
+
+class Startstop(models.Model):
+	datetimestart = models.TimeField()
+	datetimestop = models.TimeField()
+	datetimeadd = models.TimeField(auto_now_add)
+	events = models.ForeignKey(Events)
+	masters = models.ForeignKey(Masters)
