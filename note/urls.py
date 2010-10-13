38b5ea95ac04d56f020196be5777014d0eb65d47
Feliks <felix.teachme@gmail.com>
2010-10-13 16:29:26 +0300

    fifth commit

--- note/urls.py
+++ note/urls.py
@@ -1,8 +1,16 @@
 from django.conf.urls.defaults import *
+from galina.note.views import *
+
+urlpatterns = patterns('',(r'^hello/$', hello),)
+urlpatterns = patterns('',(r'^current_url_view_good/$', current_url_view_good),)
+urlpatterns = patterns('',(r'^ua_display_good2/$', ua_display_good2),)
+urlpatterns = patterns('',(r'^display_meta/$', display_meta),)
+'''
+from django.conf.urls.defaults import *
 from galina.note.views import current_datetime
 
 urlpatterns = patterns('',(r'^$', current_datetime),)
-
+'''
 ''' 
 from django.conf.urls.defaults import *
 
--- note/views.py
+++ note/views.py
@@ -1,10 +1,32 @@
+from django.http import HttpResponse
+
+def hello(request):
+    return HttpResponse("Hello world")
+
+def current_url_view_good(request):
+    return HttpResponse("Welcome to the page at %s" % request.path)
+
+def ua_display_good2(request):
+    ua = request.META.get('HTTP_USER_AGENT', 'unknown')
+    return HttpResponse("Your browser is %s" % ua)
+
+def display_meta(request):
+    values = request.META.items()
+    values.sort()
+    html = []
+    for k, v in values:
+        html.append('<tr><td>%s</td><td>%s</td></tr>' % (k, v))
+    return HttpResponse('<table>%s</table>' % '\n'.join(html))
+
+
+'''
 from django.shortcuts import render_to_response
 import datetime
 
 def current_datetime(request):
     now = datetime.datetime.now()
     return render_to_response('note.html', {'current_date': now})
-
+'''
 '''
 from django.template.loader import get_template
 from django.template import Context
